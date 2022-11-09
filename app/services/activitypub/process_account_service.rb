043f946564a31c5fc7bdb1b4140a3d36037eae84
Eugen Rochko <eugen@zeonfederated.com>
2022-11-09 08:24:21 +0100

    Fix being able to spoof link verification (#20217)
    - Change verification to happen in `default` queue
    - Change verification worker to only be queued if there's something to do
    - Add `link` tags from metadata fields to page header of profiles
    
    Signed-off-by: PolyamSpace <117664621+polyamspace@users.noreply.github.com>

--- app/services/activitypub/process_account_service.rb
+++ app/services/activitypub/process_account_service.rb
@@ -39,7 +39,8 @@ class ActivityPub::ProcessAccountService < BaseService
 
     unless @options[:only_key] || @account.suspended?
       check_featured_collection! if @account.featured_collection_url.present?
-      check_links! unless @account.fields.empty?
+      check_featured_tags_collection! if @json['featuredTags'].present?
+      check_links! if @account.fields.any?(&:requires_verification?)
     end
 
     @account
