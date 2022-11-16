7f1daaaad480d7e1fd91224ddb620ca9247cac6e
PolyamSpace <117664621+polyamspace@users.noreply.github.com>
2022-11-16 15:13:41 +0100

    Customize version
    Signed-off-by: PolyamSpace <117664621+polyamspace@users.noreply.github.com>

--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      '+glitch'
+      '+glitch+polyam'
     end
 
     def to_a
@@ -33,7 +33,7 @@ module Mastodon
     end
 
     def repository
-      ENV.fetch('GITHUB_REPOSITORY', 'glitch-soc/mastodon')
+      ENV.fetch('GITHUB_REPOSITORY', 'polyamspace/mastodon')
     end
 
     def source_base_url
