366f5d9bd33f099752fbc978b106d1944a1b84a7
PolyamSpace <117664621+polyamspace@users.noreply.github.com>
2022-12-11 10:27:54 +0100

    Add source file options to maintain transparency
    Fixes converted pngs having a white background added to them when uploading an svg.
    
    Turns out this argument has to be set before the file input in the convert command.
    This is more or less a hotfix, which works for now.
    Doesn't mess with other upload types.
    
    A better solution would be to have different options for different file types, as `media_attachment.rb` has to also fix images not scaling to the required size.
    
    Signed-off-by: PolyamSpace <117664621+polyamspace@users.noreply.github.com>

--- app/models/site_upload.rb
+++ app/models/site_upload.rb
@@ -52,7 +52,7 @@ class SiteUpload < ApplicationRecord
 
   STYLES[:favicon].freeze
 
-  has_attached_file :file, styles: ->(file) { STYLES[file.instance.var.to_sym] }, convert_options: { all: '-coalesce +profile "!icc,*" +set modify-date +set create-date' }, processors: [:lazy_thumbnail, :blurhash_transcoder, :type_corrector]
+  has_attached_file :file, source_file_options: { all: '-background transparent' }, styles: ->(file) { STYLES[file.instance.var.to_sym] }, convert_options: { all: '-coalesce +profile "!icc,*" +set modify-date +set create-date' }, processors: [:lazy_thumbnail, :blurhash_transcoder, :type_corrector]
 
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
   validates :file, presence: true
