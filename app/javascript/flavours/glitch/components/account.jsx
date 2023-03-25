2886aaa09ef6878c0fdbdb11c7a82498f733d394
Eugen Rochko <eugen@zeonfederated.com>
2023-03-25 08:46:32 +0100

    [Polyam] Change design of account rows in web UI
    Port ef127c964a60f365129bc97a2cb2fc6d12ba6407 to polyam-glitch
    
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/glitch/components/account.jsx
+++ app/javascript/flavours/glitch/components/account.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { PureComponent } from 'react';
 
 import { defineMessages, injectIntl } from 'react-intl';
 
@@ -9,10 +10,13 @@ import { Skeleton } from 'flavours/glitch/components/skeleton';
 import { me } from 'flavours/glitch/initial_state';
 
 import { Avatar } from './avatar';
+import { FollowersCounter } from './counters';
 import { DisplayName } from './display_name';
+import { Icon } from './icon';
 import { IconButton } from './icon_button';
 import Permalink from './permalink';
 import { RelativeTimestamp } from './relative_timestamp';
+import { ShortNumber } from './short_number';
 
 
 const messages = defineMessages({
@@ -26,6 +30,26 @@ const messages = defineMessages({
   mute: { id: 'account.mute', defaultMessage: 'Mute @{name}' },
   block: { id: 'account.block', defaultMessage: 'Block @{name}' },
 });
+
+class VerifiedBadge extends PureComponent {
+
+  static propTypes = {
+    link: PropTypes.string.isRequired,
+    verifiedAt: PropTypes.string.isRequired,
+  };
+
+  render () {
+    const { link } = this.props;
+
+    return (
+      <span className='verified-badge'>
+        <Icon id='check' className='verified-badge__mark' />
+        <span dangerouslySetInnerHTML={{ __html: link }} />
+      </span>
+    );
+  }
+
+}
 
 class Account extends ImmutablePureComponent {
 
@@ -92,7 +116,11 @@ class Account extends ImmutablePureComponent {
           <div className='account__wrapper'>
             <div className='account__display-name'>
               <div className='account__avatar-wrapper'><Skeleton width={36} height={36} /></div>
-              <DisplayName />
+
+              <div>
+                <DisplayName />
+                <Skeleton width='7ch' />
+              </div>
             </div>
           </div>
         </div>
@@ -146,9 +174,18 @@ class Account extends ImmutablePureComponent {
       }
     }
 
-    let mute_expires_at;
+    let muteTimeRemaining;
+
     if (account.get('mute_expires_at')) {
-      mute_expires_at =  <div><RelativeTimestamp timestamp={account.get('mute_expires_at')} futureDate /></div>;
+      muteTimeRemaining = <>· <RelativeTimestamp timestamp={account.get('mute_expires_at')} futureDate /></>;
+    }
+
+    let verification;
+
+    const firstVerifiedField = account.get('fields').find(item => !!item.get('verified_at'));
+
+    if (firstVerifiedField) {
+      verification = <>· <VerifiedBadge link={firstVerifiedField.get('value')} verifiedAt={firstVerifiedField.get('verified_at')} /></>;
     }
 
     return small ? (
@@ -172,9 +209,14 @@ class Account extends ImmutablePureComponent {
       <div className='account'>
         <div className='account__wrapper'>
           <Permalink key={account.get('id')} className='account__display-name' title={account.get('acct')} href={account.get('url')} to={`/@${account.get('acct')}`}>
-            <div className='account__avatar-wrapper'><Avatar account={account} size={size} /></div>
-            {mute_expires_at}
-            <DisplayName account={account} />
+            <div className='account__avatar-wrapper'>
+              <Avatar account={account} size={size} />
+            </div>
+
+            <div>
+              <DisplayName account={account} />
+              <ShortNumber value={account.get('followers_count')} renderer={FollowersCounter} /> {verification} {muteTimeRemaining}
+            </div>
           </Permalink>
           {buttons ?
             <div className='account__relationship'>
