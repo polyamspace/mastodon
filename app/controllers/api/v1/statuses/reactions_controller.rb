8bcbcadd7a8aca63390a15b829d9e6d69024cb25
Jeremy Kescher <jeremy@kescher.at>
2023-05-07 23:27:19 +0200

    Reactions: Return 404 when status should not be visible, asynchronous unreact

--- app/controllers/api/v1/statuses/reactions_controller.rb
+++ app/controllers/api/v1/statuses/reactions_controller.rb
@@ -5,21 +5,35 @@ class Api::V1::Statuses::ReactionsController < Api::BaseController
 
   before_action -> { doorkeeper_authorize! :write, :'write:favourites' }
   before_action :require_user!
-  before_action :set_status
+  before_action :set_status, only: [:create]
 
   def create
     ReactService.new.call(current_account, @status, params[:id])
-    render_empty
+    render json: @status, serializer: REST::StatusSerializer
   end
 
   def destroy
-    UnreactService.new.call(current_account, @status, params[:id])
-    render_empty
+    react = current_account.status_reactions.find_by(status_id: params[:status_id], name: params[:id])
+
+    if react
+      @status = react.status
+      UnreactWorker.perform_async(current_account.id, @status.id, params[:id])
+    else
+      @status = Status.find(params[:status_id])
+      authorize @status, :show?
+    end
+
+    render json: @status, serializer: REST::StatusSerializer, relationships: StatusRelationshipsPresenter.new([@status], current_account.id, reactions_map: { @status.id => false })
+  rescue Mastodon::NotPermittedError
+    not_found
   end
 
   private
 
   def set_status
     @status = Status.find(params[:status_id])
+    authorize @status, :show?
+  rescue Mastodon::NotPermittedError
+    not_found
   end
 end
