b9da0e27d0ce46fc485facc27da3b878faa0a518
PolyamSpace <117664621+polyamspace@users.noreply.github.com>
2023-05-09 22:22:13 +0200

    Fix lint errors
    Signed-off-by: PolyamSpace <117664621+polyamspace@users.noreply.github.com>

--- db/migrate/20230222143034_add_reactions_count.rb
+++ db/migrate/20230222143034_add_reactions_count.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddReactionsCount < ActiveRecord::Migration[6.1]
   def change
     add_column :status_stats, :reactions_count, :bigint, null: false, default: 0
