426ec5e1555742637c06bcb65c719306bfc021ff
PolyamSpace <117664621+polyamspace@users.noreply.github.com>
2023-05-10 14:23:14 +0200

    Add missing content in `undo_spec.rb`
    This failed, because of the missing content attribute in json.
    
    Signed-off-by: PolyamSpace <117664621+polyamspace@users.noreply.github.com>

--- spec/lib/activitypub/activity/undo_spec.rb
+++ spec/lib/activitypub/activity/undo_spec.rb
@@ -184,6 +184,7 @@ RSpec.describe ActivityPub::Activity::Undo do
         {
           id: 'bar',
           type: 'EmojiReact',
+          content: '👍',
           actor: ActivityPub::TagManager.instance.uri_for(sender),
           object: ActivityPub::TagManager.instance.uri_for(status),
         }
