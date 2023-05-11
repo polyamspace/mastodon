5430786905cfceae6ebea084b5a8a98d09cdf2d4
Jeremy Kescher <jeremy@kescher.at>
2023-05-11 13:40:24 +0200

    ReactionsController: Don't check for status reaction existence in destroy
    UnreactService checks for its existence in the background anyway, so remove redundant checks.

--- app/controllers/api/v1/statuses/reactions_controller.rb
+++ app/controllers/api/v1/statuses/reactions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Statuses::ReactionsController < Api::BaseController
 
   before_action -> { doorkeeper_authorize! :write, :'write:favourites' }
   before_action :require_user!
-  before_action :set_status, only: [:create]
+  before_action :set_status
 
   def create
     ReactService.new.call(current_account, @status, params[:id])
@@ -13,15 +13,7 @@ class Api::V1::Statuses::ReactionsController < Api::BaseController
   end
 
   def destroy
-    react = current_account.status_reactions.find_by(status_id: params[:status_id], name: params[:id])
-
-    if react
-      @status = react.status
-      UnreactWorker.perform_async(current_account.id, @status.id, params[:id])
-    else
-      @status = Status.find(params[:status_id])
-      authorize @status, :show?
-    end
+    UnreactWorker.perform_async(current_account.id, @status.id, params[:id])
 
     render json: @status, serializer: REST::StatusSerializer, relationships: StatusRelationshipsPresenter.new([@status], current_account.id, reactions_map: { @status.id => false })
   rescue Mastodon::NotPermittedError
