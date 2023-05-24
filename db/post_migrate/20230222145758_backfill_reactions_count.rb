f9345799342acc6c273d26642e1765d605cc00d8
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2023-05-24 19:47:42 +0200

    Change backfilling to post deploy migration
    Also batches it.
    
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

---
+++ db/post_migrate/20230222145758_backfill_reactions_count.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class BackfillReactionsCount < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
+  class StatusReaction < ApplicationRecord; end
+
+  def up
+    safety_assured do
+      StatusReaction.unscoped.in_batches do |reactions|
+        execute <<-SQL.squish
+          INSERT INTO status_stats (status_id, reactions_count, created_at, updated_at)
+          SELECT DISTINCT ON (status_id) status_id, count(*), MIN(created_at) AS created_at, MAX(updated_at) AS updated_at
+          FROM status_reactions WHERE status_id IN (#{reactions.map(&:status_id).uniq.join(', ')}) GROUP BY status_id
+          ON CONFLICT (status_id) DO UPDATE
+          SET reactions_count = EXCLUDED.reactions_count, created_at = LEAST(status_stats.created_at, EXCLUDED.created_at), updated_at = GREATEST(status_stats.updated_at, EXCLUDED.updated_at)
+        SQL
+      end
+    end
+  end
+
+  def down; end
+end
