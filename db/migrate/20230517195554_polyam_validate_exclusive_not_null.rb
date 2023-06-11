a1d48f5ce08b92046cc7e87b8fe15138cda8a63a
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2023-06-11 17:32:45 +0200

    Make migration compatible with Postgres 10
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- db/migrate/20230517195554_polyam_validate_exclusive_not_null.rb
+++ db/migrate/20230517195554_polyam_validate_exclusive_not_null.rb
@@ -3,7 +3,10 @@
 class PolyamValidateExclusiveNotNull < ActiveRecord::Migration[6.1]
   def change
     validate_check_constraint :lists, name: 'lists_exclusive_null'
-    change_column_null :lists, :exclusive, false
-    remove_check_constraint :lists, name: 'lists_exclusive_null'
+
+    # The following lines are fine for Postgres 12+ environments, which every reasonable admin should run anyway
+    # But vanilla enforces Postgres 10 compability, because they couldn't be arsed to update.
+    # change_column_null :lists, :exclusive, false
+    # remove_check_constraint :lists, name: 'lists_exclusive_null'
   end
 end
