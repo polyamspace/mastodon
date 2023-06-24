c4baab54b4cb6cf6837a9b5161e9fc0960c3beea
Claire <claire.github-309c@sitedethib.com>
2023-06-24 17:24:31 +0200

    [Polyam] Fix verified badge in account lists potentially including rel="me" links
    Port 55e7c08a83547424024bac311d5459cb82cf6dae to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/verified_badge.tsx
+++ app/javascript/flavours/polyam/components/verified_badge.tsx
@@ -4,12 +4,28 @@ import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 import { Icon } from './icon';
 
+const domParser = new DOMParser();
+
+const stripRelMe = (html: string) => {
+  const document = domParser.parseFromString(html, 'text/html').documentElement;
+
+  document.querySelectorAll<HTMLAnchorElement>('a[rel]').forEach((link) => {
+    link.rel = link.rel
+      .split(' ')
+      .filter((x: string) => x !== 'me')
+      .join(' ');
+  });
+
+  const body = document.querySelector('body');
+  return body ? { __html: body.innerHTML } : undefined;
+};
+
 interface Props {
   link: string;
 }
 export const VerifiedBadge: React.FC<Props> = ({ link }) => (
   <span className='verified-badge'>
     <Icon id='check' icon={faCheck} className='verified-badge__mark' />
-    <span dangerouslySetInnerHTML={{ __html: link }} />
+    <span dangerouslySetInnerHTML={stripRelMe(link)} />
   </span>
 );
