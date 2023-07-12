648a0441df82733b425bac8f7f478ed299c937b8
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2023-07-13 00:04:19 +0200

    [Vanilla] Load custom sounds from asset host
    This is neither pretty nor am I happy with it, but it works.
    
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/mastodon/initial_state.js
+++ app/javascript/mastodon/initial_state.js
@@ -64,7 +64,7 @@
  * @property {number} max_reactions
  * @property {string=} me
  * @property {string=} moved_to_account_id
- * @property {unknown} notification_sound
+ * @property {{src: string, type: string}[]} notification_sound
  * @property {string=} owner
  * @property {boolean} profile_directory
  * @property {boolean} registrations_open
