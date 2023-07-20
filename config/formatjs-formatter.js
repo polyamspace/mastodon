43583b8dc62f6af62d0c00f0c6fe818e36680e2f
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2023-07-20 19:22:20 +0200

    Change formatjs to write out to `polyam/locales/en.json`
    Inherit locales from vanilla and glitch
    
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- config/formatjs-formatter.js
+++ config/formatjs-formatter.js
@@ -1,12 +1,13 @@
 const path = require('path');
 
-const upstreamTranslations = require(path.join(__dirname, "../app/javascript/mastodon/locales/en.json"));
-const currentTranslations = require(path.join(__dirname, "../app/javascript/flavours/glitch/locales/en.json"));
+const vanillaTranslations = require(path.join(__dirname, "../app/javascript/mastodon/locales/en.json"));
+const upstreamTranslations = require(path.join(__dirname, "../app/javascript/flavours/glitch/locales/en.json"))
+const currentTranslations = require(path.join(__dirname, "../app/javascript/flavours/polyam/locales/en.json"));
 
 exports.format = (msgs) => {
   const results = {};
   for (const [id, msg] of Object.entries(msgs)) {
-    if (!upstreamTranslations[id]) {
+    if (!vanillaTranslations[id] && !upstreamTranslations[id]) {
       results[id] = currentTranslations[id] || msg.defaultMessage;
     }
   }
