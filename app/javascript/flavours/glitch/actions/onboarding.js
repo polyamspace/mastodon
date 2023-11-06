40aef10b6664263f77e5a9dcc01b9b46151dd103
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2023-11-06 05:36:31 +0100

    Re-add onboarding actions
    These were deleted, probably because unused, but the new onboarding requires these.
    
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/glitch/actions/onboarding.js
+++ app/javascript/flavours/glitch/actions/onboarding.js
@@ -1,6 +1,13 @@
 import { openModal } from './modal';
 import { changeSetting, saveSettings } from './settings';
 
+export const INTRODUCTION_VERSION = 20181216044202;
+
+export const closeOnboarding = () => dispatch => {
+  dispatch(changeSetting(['introductionVersion'], INTRODUCTION_VERSION));
+  dispatch(saveSettings());
+};
+
 export function showOnboardingOnce() {
   return (dispatch, getState) => {
     const alreadySeen = getState().getIn(['settings', 'onboarded']);
