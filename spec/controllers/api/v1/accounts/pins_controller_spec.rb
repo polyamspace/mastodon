a4e954a2eedd511ead43bd4523e34f8a37e0611f
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2023-11-19 18:19:35 +0100

    Update pins_controller_spec.rb
    new tests
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- spec/controllers/api/v1/accounts/pins_controller_spec.rb
+++ spec/controllers/api/v1/accounts/pins_controller_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Accounts::PinsController do
+  # What's this for?
+  render_views
+
+  let(:account) { Fabricate(:account) }
   let(:john)  { Fabricate(:user) }
   let(:kevin) { Fabricate(:user) }
   let(:token) { Fabricate(:accessible_access_token, resource_owner_id: john.id, scopes: 'write:accounts') }
@@ -10,6 +14,34 @@ RSpec.describe Api::V1::Accounts::PinsController do
   before do
     kevin.account.followers << john.account
     allow(controller).to receive(:doorkeeper_token) { token }
+  end
+
+  describe 'GET #index' do
+    before do
+      account.follow!(kevin.account)
+      Fabricate(:account_pin, account: account, target_account: kevin.account)
+    end
+
+    it 'returns accounts pinned by the given account', :aggregate_failures do
+      get :index, params: { account_id: account.id, limit: 1 }
+
+      expect(response).to have_http_status(200)
+      expect(body_as_json.size).to eq 1
+      expect(body_as_json[0][:id]).to eq kevin.account.id.to_s
+    end
+
+    context 'when requesting user is blocked' do
+      before do
+        account.block!(john.account)
+      end
+
+      it 'hides results' do
+        get :index, params: { account_id: account.id, limit: 1 }
+
+        expect(response).to have_http_status(200)
+        expect(body_as_json.size).to eq 0
+      end
+    end
   end
 
   describe 'POST #create' do
