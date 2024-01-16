07226b1e3f9ecb8969b9b298dda6be7040a54ed1
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2024-01-16 19:01:57 +0100

    Fix imports of additional components in glitch flavour
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/glitch/features/reactions/index.jsx
+++ app/javascript/flavours/glitch/features/reactions/index.jsx
@@ -8,10 +8,10 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import { connect } from 'react-redux';
 
-import { ReactComponent as ReactIcon } from '@material-symbols/svg-600/outlined/add.svg';
-import { ReactComponent as RefreshIcon } from '@material-symbols/svg-600/outlined/refresh.svg';
 import { debounce } from 'lodash';
 
+import ReactIcon from '@/material-icons/400-24px/add.svg?react';
+import RefreshIcon from '@/material-icons/400-24px/refresh.svg';
 import { fetchReactions, expandReactions } from 'flavours/glitch/actions/interactions';
 import ColumnHeader from 'flavours/glitch/components/column_header';
 import { Icon } from 'flavours/glitch/components/icon';
