9b7f7734d0de2164a95adce72a950a03d4b9a8e9
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2024-02-06 22:44:13 +0100

    Fix status reaction validator spec
    This spec used the `reactions` object instead of the underlying `status_reactions` association.
    
    This made the spec fail, because the reaction object is supposed to be an array.
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- spec/validators/status_reaction_validator_spec.rb
+++ spec/validators/status_reaction_validator_spec.rb
@@ -9,30 +9,30 @@ describe StatusReactionValidator do
 
   describe '#validate' do
     it 'adds error when not a valid unicode emoji' do
-      reaction = status.reactions.build(name: 'F', account: account)
+      reaction = status.status_reactions.build(name: 'F', account: account)
       subject.validate(reaction)
       expect(reaction.errors).to_not be_empty
     end
 
     it 'does not add error when non-unicode emoji is a custom emoji' do
       custom_emoji = Fabricate(:custom_emoji)
-      reaction = status.reactions.build(name: custom_emoji.shortcode, custom_emoji: custom_emoji, account: account)
+      reaction = status.status_reactions.build(name: custom_emoji.shortcode, custom_emoji: custom_emoji, account: account)
       subject.validate(reaction)
       expect(reaction.errors).to be_empty
     end
 
     it 'adds error when local account already reacted' do
-      status.reactions.create!(name: '👍', account: account)
+      status.status_reactions.create!(name: '👍', account: account)
 
-      reaction = status.reactions.build(name: '😘', account: account)
+      reaction = status.status_reactions.build(name: '😘', account: account)
       subject.validate(reaction)
       expect(reaction.errors).to_not be_empty
     end
 
     it 'does not add error when remote account already reacted' do
-      status.reactions.create!(name: '👍', account: remote_account)
+      status.status_reactions.create!(name: '👍', account: remote_account)
 
-      reaction = status.reactions.build(name: '😘', account: remote_account)
+      reaction = status.status_reactions.build(name: '😘', account: remote_account)
       subject.validate(reaction)
       expect(reaction.errors).to be_empty
     end
