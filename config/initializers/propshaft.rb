53b73a6c19557b46812d94d2076d9298fcd74886
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2024-04-14 22:50:40 +0200

    Add current draft for material icons.
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- config/initializers/propshaft.rb
+++ config/initializers/propshaft.rb
@@ -1,3 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.config.assets.paths << Rails.root.join('app', 'javascript', 'images')
+
+# Material Design icons
+Rails.application.config.assets.paths << Rails.root.join('app', 'javascript', 'material-icons')
