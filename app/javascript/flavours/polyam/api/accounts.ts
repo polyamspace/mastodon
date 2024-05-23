cc3e516379c8af17e5c8367ea5cb4abc8de7b5a4
Renaud Chaput <renchap@gmail.com>
2024-05-23 20:22:42 +0200

    [Polyam] Fix `createDataLoadingThunk` and related actions
    Port b6fd14f0e2842eca269ef8962e3c5bd560a76357 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/api/accounts.ts
+++ app/javascript/flavours/polyam/api/accounts.ts
@@ -2,6 +2,6 @@ import { apiRequest } from 'flavours/polyam/api';
 import type { ApiRelationshipJSON } from 'flavours/polyam/api_types/relationships';
 
 export const apiSubmitAccountNote = (id: string, value: string) =>
-  apiRequest<ApiRelationshipJSON>('post', `/api/v1/accounts/${id}/note`, {
+  apiRequest<ApiRelationshipJSON>('post', `v1/accounts/${id}/note`, {
     comment: value,
   });
