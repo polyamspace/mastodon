9f36bd1484b61f03227585ae57053a51d0fa3dd8
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2024-06-18 23:37:30 +0200

    Remove additional `/` from notification policy requests
    `apiRequest` already adds a `/` after "/api", which results in the requests being `/api//v1/notifications/policy`
    
    That still worked somehow, but I'd rather have correct URLs
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/api/notification_policies.ts
+++ app/javascript/flavours/polyam/api/notification_policies.ts
@@ -2,9 +2,9 @@ import { apiRequest } from 'flavours/polyam/api';
 import type { NotificationPolicyJSON } from 'mastodon/api_types/notification_policies';
 
 export const apiGetNotificationPolicy = () =>
-  apiRequest<NotificationPolicyJSON>('GET', '/v1/notifications/policy');
+  apiRequest<NotificationPolicyJSON>('GET', 'v1/notifications/policy');
 
 export const apiUpdateNotificationsPolicy = (
   policy: Partial<NotificationPolicyJSON>,
 ) =>
-  apiRequest<NotificationPolicyJSON>('PUT', '/v1/notifications/policy', policy);
+  apiRequest<NotificationPolicyJSON>('PUT', 'v1/notifications/policy', policy);
