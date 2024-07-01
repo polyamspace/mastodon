ecf149cf5b2dd1a6454cf169eda33bea0ffa8005
Eugen Rochko <eugen@zeonfederated.com>
2024-07-01 20:10:22 +0200

    [Polyam] Fix missing confirmation when unfollowing from hover card in web UI
    Port d3f504245cab5a9a0e89262e0a1398d035dffac9 to polyam-glitch
    
    Kept different confirmation message for withdrawing follow request
    Rejected changes in `features/account/components/header.jsx`
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/follow_button.tsx
+++ app/javascript/flavours/polyam/components/follow_button.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect } from 'react';
 
-import { useIntl, defineMessages } from 'react-intl';
+import { useIntl, defineMessages, FormattedMessage } from 'react-intl';
 
 import { useIdentity } from '@/flavours/polyam/identity_context';
 import {
@@ -11,7 +11,7 @@ import {
 import { openModal } from 'flavours/polyam/actions/modal';
 import { Button } from 'flavours/polyam/components/button';
 import { LoadingIndicator } from 'flavours/polyam/components/loading_indicator';
-import { me } from 'flavours/polyam/initial_state';
+import { me, unfollowModal } from 'flavours/polyam/initial_state';
 import { useAppDispatch, useAppSelector } from 'flavours/polyam/store';
 
 const messages = defineMessages({
@@ -64,11 +64,33 @@ export const FollowButton: React.FC<{
     if (accountId === me) {
       return;
     } else if (relationship.following || relationship.requested) {
-      dispatch(unfollowAccount(accountId));
+      // Polyam: Keep unfollow modal optional
+      if (unfollowModal) {
+        dispatch(
+          openModal({
+            modalType: 'CONFIRM',
+            modalProps: {
+              message: (
+                <FormattedMessage
+                  id='confirmations.unfollow.message'
+                  defaultMessage='Are you sure you want to unfollow {name}?'
+                  values={{ name: <strong>@{account?.acct}</strong>}}
+                />
+              ),
+              confirm: intl.formatMessage(messages.unfollow),
+              onConfirm: () => {
+                dispatch(unfollowAccount(accountId));
+              },
+            },
+          }),
+        );
+      } else {
+        dispatch(unfollowAccount(accountId));
+      }
     } else {
       dispatch(followAccount(accountId));
     }
-  }, [dispatch, accountId, relationship, account, signedIn]);
+  }, [dispatch, intl, accountId, relationship, account, signedIn]);
 
   let label;
 
@@ -79,6 +101,7 @@ export const FollowButton: React.FC<{
   } else if (!relationship) {
     label = <LoadingIndicator />;
   } else if (relationship.requested) {
+    // Polyam: Kept from upstream as otherwise confusing
     label = intl.formatMessage(messages.cancel_follow_request);
   } else if (!relationship.following && relationship.followed_by) {
     label = intl.formatMessage(messages.followBack);
