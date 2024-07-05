085a4a5bc9d0a71c26b74515e2b64c2784a1d663
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2024-07-05 20:53:53 +0200

    Add `with_reblogs` and `with_replies` params to public controller
    Add support for `with_reblogs` and `with_replies` params which when truthy and the setting enabled, control whether boosts and/or replies are included in public timelines.
    
    This has the benefit that it in theory works with third-party clients and allows users to turn these off even when enabled server-wide.
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/controllers/api/v1/timelines/public_controller.rb
+++ app/controllers/api/v1/timelines/public_controller.rb
@@ -3,7 +3,7 @@
 class Api::V1::Timelines::PublicController < Api::V1::Timelines::BaseController
   before_action -> { authorize_if_got_token! :read, :'read:statuses' }
 
-  PERMITTED_PARAMS = %i(local remote limit only_media allow_local_only).freeze
+  PERMITTED_PARAMS = %i(local remote limit only_media allow_local_only with_replies with_reblogs).freeze
 
   def show
     cache_if_unauthenticated!
@@ -37,8 +37,8 @@ class Api::V1::Timelines::PublicController < Api::V1::Timelines::BaseController
       remote: truthy_param?(:remote),
       only_media: truthy_param?(:only_media),
       allow_local_only: truthy_param?(:allow_local_only),
-      with_replies: Setting.show_replies_in_public_timelines,
-      with_reblogs: Setting.show_reblogs_in_public_timelines
+      with_replies: truthy_param?(:with_replies) && Setting.show_replies_in_public_timelines,
+      with_reblogs: truthy_param?(:with_reblogs) && Setting.show_reblogs_in_public_timelines
     )
   end
 
