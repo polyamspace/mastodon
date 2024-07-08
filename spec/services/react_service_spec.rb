de7767a6f65c5ec5f1763b7c38ff5118b56db1e9
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2024-07-09 00:32:57 +0200

    Rename `sidekiq_inline` to `inline_jobs` in react specs
    Upstream renamed this in f1300ad284d8c7877a7406e253cf7558596d9837
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- spec/services/react_service_spec.rb
+++ spec/services/react_service_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe ReactService, :sidekiq_inline, type: :service do
+RSpec.describe ReactService, :inline_jobs, type: :service do
   subject { described_class.new }
 
   let(:sender) { Fabricate(:account, username: 'alice') }
