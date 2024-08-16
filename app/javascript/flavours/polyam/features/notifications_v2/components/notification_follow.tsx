ff4f38e7148a5c55760e5d6a6619e9b088b95fe8
Renaud Chaput <renchap@gmail.com>
2024-08-16 12:00:59 +0200

    [Polyam] Add a follow button & follower count on follow notifications
    Port 32a78e56e0a9934f65b89a455f2d118642b998de to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/notifications_v2/components/notification_follow.tsx
+++ app/javascript/flavours/polyam/features/notifications_v2/components/notification_follow.tsx
@@ -1,7 +1,11 @@
 import { FormattedMessage } from 'react-intl';
 
 import PersonAddIcon from '@/awesome-icons/solid/user-plus.svg?react';
+import { FollowersCounter } from 'flavours/polyam/components/counters';
+import { FollowButton } from 'flavours/polyam/components/follow_button';
+import { ShortNumber } from 'flavours/polyam/components/short_number';
 import type { NotificationGroupFollow } from 'flavours/polyam/models/notification_group';
+import { useAppSelector } from 'flavours/polyam/store';
 
 import type { LabelRenderer } from './notification_group_with_status';
 import { NotificationGroupWithStatus } from './notification_group_with_status';
@@ -14,18 +18,47 @@ const labelRenderer: LabelRenderer = (values) => (
   />
 );
 
+const FollowerCount: React.FC<{ accountId: string }> = ({ accountId }) => {
+  const account = useAppSelector((s) => s.accounts.get(accountId));
+
+  if (!account) return null;
+
+  return (
+    <ShortNumber value={account.followers_count} renderer={FollowersCounter} />
+  );
+};
+
 export const NotificationFollow: React.FC<{
   notification: NotificationGroupFollow;
   unread: boolean;
-}> = ({ notification, unread }) => (
-  <NotificationGroupWithStatus
-    type='follow'
-    icon={PersonAddIcon}
-    iconId='person-add'
-    accountIds={notification.sampleAccountIds}
-    timestamp={notification.latest_page_notification_at}
-    count={notification.notifications_count}
-    labelRenderer={labelRenderer}
-    unread={unread}
-  />
-);
+}> = ({ notification, unread }) => {
+  let actions: JSX.Element | undefined;
+  let additionalContent: JSX.Element | undefined;
+
+  if (notification.sampleAccountIds.length === 1) {
+    // only display those if the group contains 1 account, otherwise it does not make sense
+    const account = notification.sampleAccountIds[0];
+
+    if (account) {
+      actions = <FollowButton accountId={notification.sampleAccountIds[0]} />;
+      // Polyam: Don't show follower count
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      additionalContent = undefined && <FollowerCount accountId={account} />;
+    }
+  }
+
+  return (
+    <NotificationGroupWithStatus
+      type='follow'
+      icon={PersonAddIcon}
+      iconId='person-add'
+      accountIds={notification.sampleAccountIds}
+      timestamp={notification.latest_page_notification_at}
+      count={notification.notifications_count}
+      labelRenderer={labelRenderer}
+      unread={unread}
+      actions={actions}
+      additionalContent={additionalContent}
+    />
+  );
+};
