c993a27921b5b3d4cb3a210d8ff25729486d8175
Nick Schonning <nschonni@gmail.com>
2024-10-01 12:26:30 -0400

    [Polyam] Enable ESLlint no-case-declarations
    Port 61a0c58dee383bf79a3b6ff6c08f82a34710d8dd to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/reducers/compose.js
+++ app/javascript/flavours/polyam/reducers/compose.js
@@ -521,7 +521,7 @@ export default function compose(state = initialState, action) {
 
         return item;
       }));
-  case INIT_MEDIA_EDIT_MODAL:
+  case INIT_MEDIA_EDIT_MODAL: {
     const media =  state.get('media_attachments').find(item => item.get('id') === action.id);
     return state.set('media_modal', ImmutableMap({
       id: action.id,
@@ -530,6 +530,7 @@ export default function compose(state = initialState, action) {
       focusY: media.getIn(['meta', 'focus', 'y'], 0),
       dirty: false,
     }));
+  }
   case COMPOSE_CHANGE_MEDIA_DESCRIPTION:
     return state.setIn(['media_modal', 'description'], action.description).setIn(['media_modal', 'dirty'], true);
   case COMPOSE_CHANGE_MEDIA_FOCUS:
@@ -584,7 +585,7 @@ export default function compose(state = initialState, action) {
       }));
   case COMPOSE_DOODLE_SET:
     return state.mergeIn(['doodle'], action.options);
-  case REDRAFT:
+  case REDRAFT: {
     do_not_federate = !!action.status.get('local_only');
     text = action.raw_text || unescapeHTML(expandMentions(action.status));
     if (do_not_federate) text = text.replace(/ ?👁\ufe0f?\u200b?$/, '');
@@ -625,6 +626,7 @@ export default function compose(state = initialState, action) {
         }));
       }
     });
+  }
   case COMPOSE_SET_STATUS:
     do_not_federate = !!action.status.get('local_only');
     text = action.text || unescapeHTML(expandMentions(action.status));
