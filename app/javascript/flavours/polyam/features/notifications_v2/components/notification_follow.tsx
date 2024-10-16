ef21980da0cfd1f86a1481c43b7a2c13f361273b
Renaud Chaput <renchap@gmail.com>
2024-10-16 10:33:11 +0200

    [Polyam] Add ability to group follow notifications in WebUI
    Port e507b4f884e57aeda39a506436c26a4b1efb1706 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/notifications_v2/components/notification_follow.tsx
+++ app/javascript/flavours/polyam/features/notifications_v2/components/notification_follow.tsx
@@ -1,16 +1,19 @@
 import { FormattedMessage } from 'react-intl';
 
+import { Link } from 'react-router-dom';
+
 import PersonAddIcon from '@/awesome-icons/solid/user-plus.svg?react';
 import { FollowersCounter } from 'flavours/polyam/components/counters';
 import { FollowIconButton } from 'flavours/polyam/components/follow_icon_button';
 import { ShortNumber } from 'flavours/polyam/components/short_number';
+import { me } from 'flavours/polyam/initial_state';
 import type { NotificationGroupFollow } from 'flavours/polyam/models/notification_group';
 import { useAppSelector } from 'flavours/polyam/store';
 
 import type { LabelRenderer } from './notification_group_with_status';
 import { NotificationGroupWithStatus } from './notification_group_with_status';
 
-const labelRenderer: LabelRenderer = (displayedName, total) => {
+const labelRenderer: LabelRenderer = (displayedName, total, seeMoreHref) => {
   if (total === 1)
     return (
       <FormattedMessage
@@ -23,10 +26,12 @@ const labelRenderer: LabelRenderer = (displayedName, total) => {
   return (
     <FormattedMessage
       id='notification.follow.name_and_others'
-      defaultMessage='{name} and {count, plural, one {# other} other {# others}} followed you'
+      defaultMessage='{name} and <a>{count, plural, one {# other} other {# others}}</a> followed you'
       values={{
         name: displayedName,
         count: total - 1,
+        a: (chunks) =>
+          seeMoreHref ? <Link to={seeMoreHref}>{chunks}</Link> : chunks,
       }}
     />
   );
@@ -46,6 +51,10 @@ export const NotificationFollow: React.FC<{
   notification: NotificationGroupFollow;
   unread: boolean;
 }> = ({ notification, unread }) => {
+  const username = useAppSelector(
+    (state) => state.accounts.getIn([me, 'username']) as string,
+  );
+
   let actions: JSX.Element | undefined;
   let additionalContent: JSX.Element | undefined;
 
@@ -72,6 +81,7 @@ export const NotificationFollow: React.FC<{
       timestamp={notification.latest_page_notification_at}
       count={notification.notifications_count}
       labelRenderer={labelRenderer}
+      labelSeeMoreHref={`/@${username}/followers`}
       unread={unread}
       actions={actions}
       additionalContent={additionalContent}
