81cb0151bb4986c376b6ee8585463db8ff6c2376
Eugen Rochko <eugen@zeonfederated.com>
2025-03-25 19:25:07 +0100

    [Polyam] Refactor alerts to TypeScript, remove `react-notification` dependency
    Port 94d71c992e0fd88fc3a3fc92b598f139c874ab3f to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/standalone/compose/index.jsx
+++ app/javascript/flavours/polyam/features/standalone/compose/index.jsx
@@ -1,12 +1,12 @@
+import { AlertsController } from 'flavours/polyam/components/alerts_controller';
 import ComposeFormContainer from 'flavours/polyam/features/compose/containers/compose_form_container';
 import LoadingBarContainer from 'flavours/polyam/features/ui/containers/loading_bar_container';
 import ModalContainer from 'flavours/polyam/features/ui/containers/modal_container';
-import NotificationsContainer from 'flavours/polyam/features/ui/containers/notifications_container';
 
 const Compose = () => (
   <>
     <ComposeFormContainer autoFocus withoutNavigation />
-    <NotificationsContainer />
+    <AlertsController />
     <ModalContainer />
     <LoadingBarContainer className='loading-bar' />
   </>
