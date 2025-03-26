e4be2d8653fac60d1e607f21324e4af2a2de2302
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2025-03-26 18:52:09 +0100

    Revert `AltTextBadge` to upstream
    Matches this component with upstream, only keeping the button fix to prevent a form submit.
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/alt_text_badge.tsx
+++ app/javascript/flavours/polyam/components/alt_text_badge.tsx
@@ -1,27 +1,78 @@
-/* Polyam: Significantly different from upstream as modal instead of overlay */
-import { useCallback } from 'react';
+import { useState, useCallback, useRef, useId } from 'react';
 
-import { openModal } from 'flavours/polyam/actions/modal';
-import { store } from 'flavours/polyam/store';
+import { FormattedMessage } from 'react-intl';
+
+import Overlay from 'react-overlays/Overlay';
+import type {
+  OffsetValue,
+  UsePopperOptions,
+} from 'react-overlays/esm/usePopper';
+
+import { useSelectableClick } from 'flavours/polyam/hooks/useSelectableClick';
+
+const offset = [0, 4] as OffsetValue;
+const popperConfig = { strategy: 'fixed' } as UsePopperOptions;
 
 export const AltTextBadge: React.FC<{
   description: string;
 }> = ({ description }) => {
-  // Polyam: Cannot use `useAppDispatch()` directly as react-redux context is not available in server rendered pages.
-  // Essentially: doing so throws an error and prevents attachments from loading.
+  const accessibilityId = useId();
+  const anchorRef = useRef<HTMLButtonElement>(null);
+  const [open, setOpen] = useState(false);
+
   const handleClick = useCallback(() => {
-    store.dispatch(
-      openModal({ modalType: 'ALTTEXT_SHOW', modalProps: { description } }),
-    );
-  }, [description]);
+    setOpen((v) => !v);
+  }, [setOpen]);
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  const [handleMouseDown, handleMouseUp] = useSelectableClick(handleClose);
 
   return (
-    <button
-      type='button'
-      className='media-gallery__alt__label'
-      onClick={handleClick}
-    >
-      ALT
-    </button>
+    <>
+      <button
+        ref={anchorRef}
+        type='button'
+        className='media-gallery__alt__label'
+        onClick={handleClick}
+        aria-expanded={open}
+        aria-controls={accessibilityId}
+      >
+        ALT
+      </button>
+
+      <Overlay
+        rootClose
+        onHide={handleClose}
+        show={open}
+        target={anchorRef.current}
+        placement='top-end'
+        flip
+        offset={offset}
+        popperConfig={popperConfig}
+      >
+        {({ props }) => (
+          <div {...props} className='hover-card-controller'>
+            <div // eslint-disable-line jsx-a11y/no-noninteractive-element-interactions
+              className='media-gallery__alt__popover dropdown-animation'
+              role='region'
+              id={accessibilityId}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+            >
+              <h4>
+                <FormattedMessage
+                  id='alt_text_badge.title'
+                  defaultMessage='Alt text'
+                />
+              </h4>
+              <p>{description}</p>
+            </div>
+          </div>
+        )}
+      </Overlay>
+    </>
   );
 };
