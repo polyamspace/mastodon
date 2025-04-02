11084f611b03ff3d5764407a8f4e573b85b291c2
Eugen Rochko <eugen@zeonfederated.com>
2025-04-02 14:31:39 +0200

    [Polyam] Refactor `<DomainBlocks>` to TypeScript
    Port 2c70c28bbba61affd32a6161387b64d391f7dd03 to polyam-glitch
    
    Co-authored-by: Claire <claire.github-309c@sitedethib.com>
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/domain.tsx
+++ app/javascript/flavours/polyam/components/domain.tsx
@@ -1,24 +1,16 @@
 import { useCallback } from 'react';
 
-import { defineMessages, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
-import UnblockIcon from '@/awesome-icons/solid/lock-open.svg?react';
 import { unblockDomain } from 'flavours/polyam/actions/domain_blocks';
 import { useAppDispatch } from 'flavours/polyam/store';
 
-import { IconButton } from './icon_button';
-
-const messages = defineMessages({
-  unblockDomain: {
-    id: 'account.unblock_domain',
-    defaultMessage: 'Unblock domain {domain}',
-  },
-});
+import { Button } from './button';
 
 export const Domain: React.FC<{
   domain: string;
 }> = ({ domain }) => {
-  const intl = useIntl();
+  //const intl = useIntl();
   const dispatch = useAppDispatch();
 
   const handleDomainUnblock = useCallback(() => {
@@ -27,20 +19,17 @@ export const Domain: React.FC<{
 
   return (
     <div className='domain'>
-      <div className='domain__wrapper'>
-        <span className='domain__domain-name'>
-          <strong>{domain}</strong>
-        </span>
+      <div className='domain__domain-name'>
+        <strong>{domain}</strong>
+      </div>
 
-        <div className='domain__buttons'>
-          <IconButton
-            active
-            icon='unlock'
-            iconComponent={UnblockIcon}
-            title={intl.formatMessage(messages.unblockDomain, { domain })}
-            onClick={handleDomainUnblock}
+      <div className='domain__buttons'>
+        <Button onClick={handleDomainUnblock}>
+          <FormattedMessage
+            id='account.unblock_domain_short'
+            defaultMessage='Unblock'
           />
-        </div>
+        </Button>
       </div>
     </div>
   );
