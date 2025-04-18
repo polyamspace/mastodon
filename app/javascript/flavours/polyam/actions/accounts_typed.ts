a318e50b77c84364aee8b5f1ca74f3d545019acf
Eugen Rochko <eugen@zeonfederated.com>
2025-04-18 09:23:34 +0200

    [Polyam] Add option to remove account from followers in web UI
    Port bed614d44e54de6a05fff3464dddfcf8649072ee to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/actions/accounts_typed.ts
+++ app/javascript/flavours/polyam/actions/accounts_typed.ts
@@ -1,7 +1,9 @@
 import { createAction } from '@reduxjs/toolkit';
 
+import { apiRemoveAccountFromFollowers } from 'flavours/polyam/api/accounts';
 import type { ApiAccountJSON } from 'flavours/polyam/api_types/accounts';
 import type { ApiRelationshipJSON } from 'flavours/polyam/api_types/relationships';
+import { createDataLoadingThunk } from 'flavours/polyam/store/typed_functions';
 
 export const revealAccount = createAction<{
   id: string;
@@ -94,4 +96,11 @@ export const unpinAccountSuccess = createAction<{
 export const fetchRelationshipsSuccess = createAction(
   'relationships/fetch/SUCCESS',
   actionWithSkipLoadingTrue<{ relationships: ApiRelationshipJSON[] }>,
+);
+
+export const removeAccountFromFollowers = createDataLoadingThunk(
+  'accounts/remove_from_followers',
+  ({ accountId }: { accountId: string }) =>
+    apiRemoveAccountFromFollowers(accountId),
+  (relationship) => ({ relationship }),
 );
