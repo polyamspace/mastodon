69a61b4d959800fe75dd759ac882c47637699aee
Claire <claire.github-309c@sitedethib.com>
2025-05-15 20:29:43 +0200

    [Polyam] Fix middle button mouse up on status header always opening status in a new tab
    Port 24d35996907ffcdf393f9938589eedced84353e4 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/status.jsx
+++ app/javascript/flavours/polyam/components/status.jsx
@@ -371,9 +371,8 @@ class Status extends ImmutablePureComponent {
     }
   };
 
-  handleMouseUp = e => {
+  handleHeaderClick = e => {
     // Only handle clicks on the empty space above the content
-
     if (e.target !== e.currentTarget && e.detail >= 1) {
       return;
     }
@@ -819,7 +818,7 @@ class Status extends ImmutablePureComponent {
             {(connectReply || connectUp || connectToRoot) && <div className={classNames('status__line', { 'status__line--full': connectReply, 'status__line--first': !status.get('in_reply_to_id') && !connectToRoot })} />}
 
             {(!muted || !isCollapsed) && (
-              <header onMouseUp={this.handleMouseUp} className='status__info'>
+              <header onClick={this.handleHeaderClick} onAuxClick={this.handleHeaderClick} className='status__info'>
                 <Permalink
                   href={status.getIn(['account', 'url'])}
                   to={`/@${status.getIn(['account', 'acct'])}`}
