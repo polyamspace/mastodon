b55e4e30ae31168a77dc28d41bc2c703febcd6d7
Eugen Rochko <eugen@zeonfederated.com>
2025-05-31 00:24:34 +0200

    [Polyam] Add icon to header of pinned posts carousel in web UI
    Port 44d71d59efd79ee723a1dec72ec77833560c45cf to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/featured_carousel.tsx
+++ app/javascript/flavours/polyam/components/featured_carousel.tsx
@@ -5,6 +5,7 @@ import {
   useLayoutEffect,
   useRef,
   useState,
+  useId,
 } from 'react';
 
 import { defineMessages, FormattedMessage, useIntl } from 'react-intl';
@@ -18,7 +19,9 @@ import { useDrag } from '@use-gesture/react';
 
 import ChevronLeftIcon from '@/awesome-icons/solid/chevron-left.svg?react';
 import ChevronRightIcon from '@/awesome-icons/solid/chevron-right.svg?react';
+import PushPinIcon from '@/awesome-icons/solid/thumbtack.svg?react';
 import { expandAccountFeaturedTimeline } from '@/flavours/polyam/actions/timelines';
+import { Icon } from '@/flavours/polyam/components/icon';
 import { IconButton } from '@/flavours/polyam/components/icon_button';
 import StatusContainer from '@/flavours/polyam/containers/status_container';
 import { usePrevious } from '@/flavours/polyam/hooks/usePrevious';
@@ -38,6 +41,7 @@ export const FeaturedCarousel: React.FC<{
   tagged?: string;
 }> = ({ accountId, tagged }) => {
   const intl = useIntl();
+  const accessibilityId = useId();
 
   // Load pinned statuses
   const dispatch = useAppDispatch();
@@ -120,11 +124,15 @@ export const FeaturedCarousel: React.FC<{
       className='featured-carousel'
       {...bind()}
       aria-roledescription='carousel'
-      aria-labelledby='featured-carousel-title'
+      aria-labelledby={`${accessibilityId}-title`}
       role='region'
     >
       <div className='featured-carousel__header'>
-        <h4 className='featured-carousel__title' id='featured-carousel-title'>
+        <h4
+          className='featured-carousel__title'
+          id={`${accessibilityId}-title`}
+        >
+          <Icon id='thumb-tack' icon={PushPinIcon} />
           <FormattedMessage
             id='featured_carousel.header'
             defaultMessage='{count, plural, one {Pinned Post} other {Pinned Posts}}'
