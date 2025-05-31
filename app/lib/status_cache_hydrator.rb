5846119d4c5bf13535dc2b6bb2a079dba1e2586b
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2025-05-31 22:08:27 +0200

    Fix lint error
    Fixes extra space complaint after reordering
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/lib/status_cache_hydrator.rb
+++ app/lib/status_cache_hydrator.rb
@@ -72,7 +72,7 @@ class StatusCacheHydrator
     payload[:pinned]     = StatusPin.exists?(account_id: account_id, status_id: status.id) if status.account_id == account_id
     payload[:filtered]   = mapped_applied_custom_filter(account_id, status)
     payload[:quote] = hydrate_quote_payload(payload[:quote], status.quote, account_id, nested:) if payload[:quote]
-    payload[:reactions]  = serialized_reactions(account_id, status)
+    payload[:reactions] = serialized_reactions(account_id, status)
   end
 
   def hydrate_quote_payload(empty_payload, quote, account_id, nested: false)
