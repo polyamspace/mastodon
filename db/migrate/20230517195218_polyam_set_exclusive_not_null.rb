25df73bf5cde00f1d6e9dc801f01bbeac9cad355
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2025-06-06 13:30:29 +0200

    Skip old exclusive lists migrations when running new code
    While testing switching between forks, I noticed that these migrations are run even on newer installs.
    
    These migrations are only necessary when polyam-glitch was run before upstream implemented this feature.
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- db/migrate/20230517195218_polyam_set_exclusive_not_null.rb
+++ db/migrate/20230517195218_polyam_set_exclusive_not_null.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+# This migration is only needed when Polyam-glitch was run before upstream implemented exclusive lists.
+# The prior code allowed the `exclusive` column to be null.
 class PolyamSetExclusiveNotNull < ActiveRecord::Migration[6.1]
   def change
+    return if column_exists?(:lists, :exclusive, null: false)
+
     add_check_constraint :lists, 'exclusive IS NOT NULL', name: 'lists_exclusive_null', validate: false
   end
 end
--- db/migrate/20230517195554_polyam_validate_exclusive_not_null.rb
+++ db/migrate/20230517195554_polyam_validate_exclusive_not_null.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+# This migration is only needed when Polyam-glitch was run before upstream implemented exclusive lists.
+# The prior code allowed the `exclusive` column to be null.
 class PolyamValidateExclusiveNotNull < ActiveRecord::Migration[6.1]
   def change
+    return unless check_constraint_exists?(:lists, name: 'lists_exclusive_null')
+
     validate_check_constraint :lists, name: 'lists_exclusive_null'
 
     # The following lines are fine for Postgres 12+ environments, which every reasonable admin should run anyway
