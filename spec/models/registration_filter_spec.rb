da3875495059e4152a4dfcdbc316f9de0f026679
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2025-06-13 15:38:00 +0200

    Fix `RSpec/EmptyExampleGroup` in registration filter (#907)
    Adds validation specs as there isn't really anything else to do.
    
    I don't know how to test the second validation though.
    
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- spec/models/registration_filter_spec.rb
+++ spec/models/registration_filter_spec.rb
@@ -3,4 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe RegistrationFilter do
+  # Polyam TODO: Figure out how to test for regex: true
+  describe 'Validations' do
+    it { is_expected.to define_enum_for(:type).with_values(text: 0, regexp: 1).with_suffix(:type) }
+    it { is_expected.to validate_presence_of(:phrase) }
+  end
 end
