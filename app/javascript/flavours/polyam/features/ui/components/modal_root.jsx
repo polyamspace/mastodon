27a1694d88e3ef82fd9ef137cb5b26872c76df16
diondiondion <mail@diondiondion.com>
2025-06-25 14:12:49 +0200

    [Polyam] fix: Prevent content scrolling behind main menu (part 1)
    Port c6dddbb66e5de5e0cd18eb0fe667c41aada764ba to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/ui/components/modal_root.jsx
+++ app/javascript/flavours/polyam/features/ui/components/modal_root.jsx
@@ -21,7 +21,6 @@ import {
   IgnoreNotificationsModal,
   AnnualReportModal,
 } from 'flavours/polyam/features/ui/util/async-components';
-import { getScrollbarWidth } from 'flavours/polyam/utils/scrollbar';
 
 import BundleContainer from '../containers/bundle_container';
 
@@ -99,16 +98,6 @@ export default class ModalRoot extends PureComponent {
   state = {
     backgroundColor: null,
   };
-
-  componentDidUpdate () {
-    if (this.props.type) {
-      document.body.classList.add('with-modals--active');
-      document.documentElement.style.marginRight = `${getScrollbarWidth()}px`;
-    } else {
-      document.body.classList.remove('with-modals--active');
-      document.documentElement.style.marginRight = '0';
-    }
-  }
 
   setBackgroundColor = color => {
     this.setState({ backgroundColor: color });
