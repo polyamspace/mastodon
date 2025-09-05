1a1fbe706a58a167b8c925b5fe75b7113e160daa
Claire <claire.github-309c@sitedethib.com>
2025-09-05 12:17:02 +0200

    [Polyam] Fix editing or deleting and redrafting polls in 4.3
    Port 055be70c5908a030196d21201fc50705a5409a19 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/reducers/compose.js
+++ app/javascript/flavours/polyam/reducers/compose.js
@@ -632,7 +632,7 @@ export default function compose(state = initialState, action) {
       }
 
       if (action.status.get('poll')) {
-        let options = ImmutableList(action.status.get('poll').options.map(x => x.title));
+        let options = action.status.getIn(['poll', 'options']).map(x => x.get('title'));
         if (options.size < action.maxOptions) {
           options = options.push('');
         }
@@ -670,7 +670,7 @@ export default function compose(state = initialState, action) {
       }
 
       if (action.status.get('poll')) {
-        let options = ImmutableList(action.status.get('poll').options.map(x => x.title));
+        let options = action.status.getIn(['poll', 'options']).map(x => x.get('title'));
         if (options.size < action.maxOptions) {
           options = options.push('');
         }
