0061f9a6994b4ee73e32997f3df233750eb78afc
Echo <ChaosExAnima@users.noreply.github.com>
2025-12-04 16:48:10 +0100

    [Glitch] Create new entrypoint for sharable Wrapstodon
    Port 0dac31dfd588e4cd866f382ed001a9535f06234a to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

---
+++ app/javascript/flavours/glitch/features/annual_report/share.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+
+import { IconLogo } from '@/flavours/glitch/components/logo';
+
+import { AnnualReport } from './index';
+import classes from './share.module.css';
+
+export const WrapstodonShare: FC = () => {
+  return (
+    <main className={classes.wrapper}>
+      <AnnualReport share={false} />
+      <footer className={classes.footer}>
+        <IconLogo className={classes.logo} />
+        Generated with ♥ by the Mastodon team
+      </footer>
+    </main>
+  );
+};
