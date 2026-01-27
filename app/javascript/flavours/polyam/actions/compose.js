ee1f2bfd0980042fe8da5aeca0bda3e3d4f0ee69
Claire <claire.github-309c@sitedethib.com>
2026-01-27 22:30:17 +0100

    [Polyam] Deprecate eye emoji in favor of a bespoke API parameter
    Port ffddcc7c1d32695e20ccde387d5ec4a0d9797289 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/polyam/actions/compose.js
+++ app/javascript/flavours/polyam/actions/compose.js
@@ -228,10 +228,6 @@ export function submitCompose(overridePrivacy = null, successCallback = undefine
       return;
     }
 
-    if (getState().getIn(['compose', 'advanced_options', 'do_not_federate'])) {
-      status = status + ' 👁️';
-    }
-
     dispatch(submitComposeRequest());
 
     // If we're editing a post with media attachments, those have not
@@ -262,6 +258,7 @@ export function submitCompose(overridePrivacy = null, successCallback = undefine
         status,
         spoiler_text,
         content_type: getState().getIn(['compose', 'content_type']),
+        local_only: getState().getIn(['compose', 'advanced_options', 'do_not_federate']),
         in_reply_to_id: getState().getIn(['compose', 'in_reply_to'], null),
         media_ids: media.map(item => item.get('id')),
         media_attributes,
