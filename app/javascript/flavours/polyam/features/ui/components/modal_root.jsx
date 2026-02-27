371d09c792ef100ec3929424722bdb59856c06ca
Echo <ChaosExAnima@users.noreply.github.com>
2026-02-27 14:36:19 +0100

    [Polyam] Profile editing: Tab display controls
    Port d69d7c0507f4e0453e032c897cb3343065798381 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/ui/components/modal_root.jsx
+++ app/javascript/flavours/polyam/features/ui/components/modal_root.jsx
@@ -107,6 +107,7 @@ export const MODAL_COMPONENTS = {
   'ACCOUNT_FIELD_OVERFLOW': () => import('@/flavours/polyam/features/account_timeline/modals/field_modal').then(module => ({ default: module.AccountFieldModal })),
   'ACCOUNT_EDIT_NAME': () => import('@/flavours/polyam/features/account_edit/components/name_modal').then(module => ({ default: module.NameModal })),
   'ACCOUNT_EDIT_BIO': () => import('@/flavours/polyam/features/account_edit/components/bio_modal').then(module => ({ default: module.BioModal })),
+  'ACCOUNT_EDIT_PROFILE_DISPLAY': () => import('@/flavours/polyam/features/account_edit/components/profile_display_modal').then(module => ({ default: module.ProfileDisplayModal })),
 };
 
 export default class ModalRoot extends PureComponent {
