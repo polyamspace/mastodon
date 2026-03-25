c9ba4a0395c0e509951cb07e770d03b07427e715
diondiondion <mail@diondiondion.com>
2026-03-25 16:01:40 +0100

    [Polyam] Render `FormattedMessage` translations into `React.Fragment` by default
    Port e3dfb7ddd6ab8d821bca6337892f5d5b42992133 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/alt_text_badge/index.tsx
+++ app/javascript/flavours/polyam/components/alt_text_badge/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useRef, useId, Fragment } from 'react';
+import { useState, useCallback, useRef, useId } from 'react';
 
 import { FormattedMessage, useIntl } from 'react-intl';
 
@@ -86,7 +86,6 @@ export const AltTextBadge: React.FC<{
                 <FormattedMessage
                   id='alt_text_badge.title'
                   defaultMessage='Alt text'
-                  tagName={Fragment}
                 />
               </h4>
 
