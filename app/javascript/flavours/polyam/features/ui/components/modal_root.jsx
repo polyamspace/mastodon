4afb6dc7a1b616913308679f53b02459f006ba0b
polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>
2026-04-26 13:49:02 +0200

    Fix accidental glitch flavour imports
    Dunno how I missed this.
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>
    Signed-off-by: polyamAdmin <117664621+polyamAdmin@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/ui/components/modal_root.jsx
+++ app/javascript/flavours/polyam/features/ui/components/modal_root.jsx
@@ -103,8 +103,8 @@ export const MODAL_COMPONENTS = {
   'COMPOSE_PRIVACY': () => Promise.resolve({ default: VisibilityModal }),
   'ACCOUNT_NOTE': () => import('@/flavours/polyam/features/account_timeline/modals/note_modal').then(module => ({ default: module.AccountNoteModal })),
   'ACCOUNT_FIELD_OVERFLOW': () => import('@/flavours/polyam/features/account_timeline/modals/field_modal').then(module => ({ default: module.AccountFieldModal })),
-  'ACCOUNT_EDIT_NAME': () => import('@/flavours/glitch/features/account_edit/components/name_modal').then(module => ({ default: module.NameModal })),
-  'ACCOUNT_EDIT_BIO': () => import('@/flavours/glitch/features/account_edit/components/bio_modal').then(module => ({ default: module.BioModal })),
+  'ACCOUNT_EDIT_NAME': () => import('@/flavours/polyam/features/account_edit/components/name_modal').then(module => ({ default: module.NameModal })),
+  'ACCOUNT_EDIT_BIO': () => import('@/flavours/polyam/features/account_edit/components/bio_modal').then(module => ({ default: module.BioModal })),
 };
 
 export default class ModalRoot extends PureComponent {
