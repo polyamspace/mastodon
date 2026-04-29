0b3efbc44239ea3007aaff8c369bb2e1df6378ec
diondiondion <mail@diondiondion.com>
2026-04-29 12:35:07 +0200

    [Polyam] Add date & correct icon to "You are in this collection" callout
    Port 614eda43ff5c662cf74bcfb17d2ddace510af6c9 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/polyam/features/collections/detail/index.tsx
+++ app/javascript/flavours/polyam/features/collections/detail/index.tsx
@@ -9,14 +9,19 @@ import { Link } from 'react-router-dom';
 import HelpIcon from '@/awesome-icons/solid/circle-question.svg?react';
 import ListAltIcon from '@/awesome-icons/solid/list-ul.svg?react';
 import ShareIcon from '@/awesome-icons/solid/share-nodes.svg?react';
-import { openModal } from '@/flavours/polyam/actions/modal';
-import { useAccountHandle } from '@/flavours/polyam/components/display_name/default';
-import type { ApiCollectionJSON } from 'flavours/polyam/api_types/collections';
+import StarIcon from '@/awesome-icons/solid/star.svg?react';
+import { openModal } from 'flavours/polyam/actions/modal';
+import type {
+  ApiCollectionJSON,
+  CollectionAccountItem,
+} from 'flavours/polyam/api_types/collections';
 import { Badge } from 'flavours/polyam/components/badge';
 import { Callout } from 'flavours/polyam/components/callout';
 import { Column } from 'flavours/polyam/components/column';
 import { ColumnHeader } from 'flavours/polyam/components/column_header';
 import { DisplayName } from 'flavours/polyam/components/display_name';
+import { useAccountHandle } from 'flavours/polyam/components/display_name/default';
+import { FormattedDateWrapper } from 'flavours/polyam/components/formatted_date';
 import { IconButton } from 'flavours/polyam/components/icon_button';
 import { Scrollable } from 'flavours/polyam/components/scrollable_list/components';
 import { useAccount } from 'flavours/polyam/hooks/useAccount';
@@ -69,13 +74,15 @@ export const AuthorNote: React.FC<{ id: string }> = ({ id }) => {
 };
 
 const RevokeControls: React.FC<{
+  currentUserCollectionItem: CollectionAccountItem;
   collection: ApiCollectionJSON;
-}> = ({ collection }) => {
+}> = ({ currentUserCollectionItem, collection }) => {
   const authorAccount = useAccount(collection.account_id);
   const confirmRevoke = useConfirmRevoke(collection);
 
   return (
     <Callout
+      icon={StarIcon}
       title={
         <FormattedMessage
           id='collections.detail.you_are_in_this_collection'
@@ -95,7 +102,14 @@ const RevokeControls: React.FC<{
         defaultMessage='{author} added you on {date}'
         values={{
           author: <DisplayName account={authorAccount} variant='simple' />,
-          date: '{date}', // TODO: Data not yet provided by API
+          date: (
+            <FormattedDateWrapper
+              value={currentUserCollectionItem.created_at}
+              day='2-digit'
+              month='short'
+              year='numeric'
+            />
+          ),
         }}
       />
     </Callout>
@@ -131,10 +145,11 @@ const CollectionHeader: React.FC<{ collection: ApiCollectionJSON }> = ({
   const history = useHistory();
 
   const isOwnCollection = account_id === me;
-  const currentUserIndex = items.findIndex(
+  const currentUserCollectionItem = items.find(
     (account) => account.account_id === me,
   );
-  const isCurrentUserInCollection = !isOwnCollection && currentUserIndex > -1;
+  const isCurrentUserInCollection =
+    !isOwnCollection && !!currentUserCollectionItem;
 
   const openShareModal = useCallback(() => {
     dispatch(
@@ -184,7 +199,12 @@ const CollectionHeader: React.FC<{ collection: ApiCollectionJSON }> = ({
       </div>
       {description && <p className={classes.description}>{description}</p>}
       {hasPendingAccounts && <PendingNote />}
-      {isCurrentUserInCollection && <RevokeControls collection={collection} />}
+      {isCurrentUserInCollection && (
+        <RevokeControls
+          currentUserCollectionItem={currentUserCollectionItem}
+          collection={collection}
+        />
+      )}
     </header>
   );
 };
