bd573bd0d2d7723769b0ba91d143abdd8971a73f
Echo <ChaosExAnima@users.noreply.github.com>
2026-07-29 10:47:43 +0200

    [Polyam] Adds useMergedRefs and useListFocus hooks
    Port 936d2694076877dae0cb6508c3b35a744f87883c to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/form_fields/combobox_field.tsx
+++ app/javascript/flavours/polyam/components/form_fields/combobox_field.tsx
@@ -17,6 +17,7 @@ import classNames from 'classnames';
 import KeyboardArrowDownIcon from '@/awesome-icons/solid/chevron-down.svg?react';
 import KeyboardArrowUpIcon from '@/awesome-icons/solid/chevron-up.svg?react';
 import SearchIcon from '@/awesome-icons/solid/magnifying-glass.svg?react';
+import { useMergedRefs } from '@/flavours/polyam/hooks/useMergedRefs';
 import { IconButton } from 'flavours/polyam/components/icon_button';
 
 import { LoadingIndicator } from '../loading_indicator';
@@ -507,17 +508,7 @@ const ComboboxWithRef = <Item extends ComboboxItem, GroupKey extends string>(
       );
     });
 
-  const mergeInputRefs = useCallback(
-    (element: HTMLInputElement | null) => {
-      setInputElement(element);
-      if (typeof ref === 'function') {
-        ref(element);
-      } else if (ref) {
-        ref.current = element;
-      }
-    },
-    [ref],
-  );
+  const mergedRef = useMergedRefs(ref, setInputElement);
 
   const id = useId();
   const listId = `${id}-list`;
@@ -543,7 +534,7 @@ const ComboboxWithRef = <Item extends ComboboxItem, GroupKey extends string>(
         onKeyDown={handleInputKeyDown}
         icon={icon ?? undefined}
         className={classNames(classes.input, className)}
-        ref={mergeInputRefs}
+        ref={mergedRef}
       />
       {hasMenuContent && (
         <IconButton
