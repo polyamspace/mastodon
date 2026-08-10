a4e8e83c8629d139d5aa61fb9fdb11f4482c085c
Echo <ChaosExAnima@users.noreply.github.com>
2026-08-10 18:24:46 +0200

    [Glitch] Composer redesign: Emoji picker
    Port eca7a8c09d1168f6939b5f253901e27b41909b3c to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/actions/compose.js
+++ app/javascript/flavours/glitch/actions/compose.js
@@ -11,7 +11,7 @@ import { emojiMartSearch } from '@/flavours/glitch/features/emoji/picker';
 import { recoverHashtags } from 'flavours/glitch/utils/hashtag';
 
 import { showAlert, showAlertForError } from './alerts';
-import { useEmoji } from './emojis';
+import { emojiUse } from './emojis';
 import { importFetchedAccounts, importFetchedStatus } from './importer';
 import { openModal } from './modal';
 import { updateTimeline } from './timelines';
@@ -657,7 +657,7 @@ export function selectComposeSuggestion(position, token, suggestion, path) {
       completion    = suggestion.native || `:${suggestion.id}:`;
       startPosition = position - 1;
 
-      dispatch(useEmoji(suggestion));
+      dispatch(emojiUse(suggestion));
     } else if (suggestion.type === 'hashtag') {
       // TODO: it could make sense to keep the “most capitalized” of the two
       const tokenName = token.slice(1); // strip leading '#'
