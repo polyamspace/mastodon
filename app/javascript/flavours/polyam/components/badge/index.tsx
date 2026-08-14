d29016b24baaaede339a1aa1eb03dda25edd50ea
diondiondion <mail@diondiondion.com>
2026-08-14 13:31:53 +0000

    [Polyam] Fix badge spacing in redesign
    Port fad36853ce17045829040f8f76506dfb51d773f2 to polyam-glitch
    
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/javascript/flavours/polyam/components/badge/index.tsx
+++ app/javascript/flavours/polyam/components/badge/index.tsx
@@ -57,7 +57,7 @@ export const Badge: FC<BadgeProps> = ({
     className={classNames(
       classes.badge,
       isRedesignEnabled() && redesignClasses.badge,
-      !icon && classes.badgeWithoutIcon,
+      !icon && [classes.badgeWithoutIcon, redesignClasses.badgeWithoutIcon],
       classes[variant],
       className,
     )}
